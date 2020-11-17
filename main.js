4ab50e162907c1bdf1389e1f9bd8b0111542200e
Tracy Falba <tfalba@mac.com>
2020-11-17 18:44:10 -0500

    added abbreviated state function and cleaned up styling

--- main.js
+++ main.js
@@ -1,19 +1,6 @@
 
 console.log('hello')
-const months = [
-  'Jan',
-  'Feb',
-  'Mar',
-  'Apr',
-  'May',
-  'Jun',
-  'Jul',
-  'Aug',
-  'Sep',
-  'Oct',
-  'Nov',
-  'Dec'
-]
+const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
 const header = document.createElement('h1')
 header.classList.add('h1')
 document.querySelector('body').appendChild(header)
@@ -35,12 +22,14 @@ for (let customer of customers) {
   const regMonthName = months[regMonthIndex]
   const regDay = registered.getDate()
   const regYear = registered.getFullYear()
+  const abbrState = nameToAbbr(customer.location.state)
+
 
   customerDiv.innerHTML = `<img src=${customer.picture['large']}>
   <div class='name'>${customer.name.first} ${customer.name.last}</div>
   <div class='email'>${customer.email}</div>
   <div class='address'>${customer.location.street.number} ${customer.location.street.name}</div>
-  <div class='address'>${customer.location.city}, ${customer.location.state} ${customer.location.postcode}</div>
+  <div class='address'>${customer.location.city}, ${abbrState} ${customer.location.postcode}</div>
   <div class='dob'>DOB: ${monthName}, ${day} ${year}</div>
   <div class='registered'>Customer since: ${regMonthName}, ${regDay} ${regYear}</div>`
 }
